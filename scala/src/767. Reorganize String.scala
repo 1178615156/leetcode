2e7714eb1ce21b20d9cd553256fe12b97a8d5ec6
yujiesui <a1178615156@gmail.com>
2019-09-05 18:13:55 +0800

    update

---
+++ scala/src/767. Reorganize String.scala
@@ -0,0 +1,22 @@
+object Solution {
+  def reorganizeString(S: String): String = {
+    val charCount  = S.toList.groupBy(e => e)
+      .mapValues(_.size)
+      .toList
+      .sortBy(e => -e._2)
+    val count      = charCount.map(e => e._2)
+    val maxCount   = count.head
+    val maxChar    = charCount.head._1
+    val otherCount = S.size - maxCount
+    val otherS     = S.toSeq.filter(_ != maxChar).sorted
+    if(otherCount >= maxCount - 1) {
+      val result = Array.fill[StringBuilder](maxCount)(new StringBuilder(maxChar.toString))
+      for (i <- 0 until otherS.length) {
+        result(i % maxCount).append(otherS(i))
+      }
+      result.foldLeft(new StringBuilder())(_.append(_)).toString()
+    } else {
+      ""
+    }
+  }
+}
