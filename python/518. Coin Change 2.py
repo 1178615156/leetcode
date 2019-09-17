eb7b0320731613d683b7f7fb384960231b095a38
yujiesui <a1178615156@gmail.com>
2019-09-17 18:18:56 +0800

    update

--- python/518. Coin Change 2.py
+++ python/518. Coin Change 2.py
@@ -6,28 +6,31 @@ class Solution:
         self.cache = dict()
 
     def change(self, amount: int, coins: List[int]) -> int:
+        return self._change(amount, sorted(coins, reverse=True))
 
-        def _change():
+    def _change(self, amount: int, coins: List[int]) -> int:
+
+        def mk_result():
             if amount == 0:
                 return 1
-            if not coins:
+            if not coins or amount < 0:
                 return 0
 
             coin = coins[0]
             if amount < coin:
-                return self.change(amount, coins[1:])
+                return self._change(amount, coins[1:])
             else:
                 return (
-                        self.change(amount, coins[1:]) +
-                        self.change(amount - coin, coins)
+                        self._change(amount, coins[1:]) +
+                        self._change(amount - coin, coins)
                 )
 
         key = (amount, id(coins))
         if not key in self.cache:
-            result = _change()
+            result = mk_result()
             self.cache[key] = result
         return self.cache[key]
 
 
 if __name__ == '__main__':
-    print(Solution().change(amount=5, coins=[1, 2, 5]))
+    print(Solution()._change(amount=5, coins=[1, 2, 5]))
